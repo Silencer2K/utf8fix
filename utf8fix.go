251c18a7bb3c8ea8ef9ea8e477b6fab9cdb55202
Silencer <silencer2k@gmail.com>
2025-07-22 09:35:50 +0300

    Add comment

--- utf8fix.go
+++ utf8fix.go
@@ -4,6 +4,17 @@ import (
 	"unicode/utf8"
 )
 
+// RFC 3629
+// https://datatracker.ietf.org/doc/html/rfc3629
+//
+// Char. number range  |        UTF-8 octet sequence
+//    (hexadecimal)    |              (binary)
+// --------------------+---------------------------------------------
+// 0000 0000-0000 007F | 0xxxxxxx
+// 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+// 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+// 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
 const (
 	headByte  = 0xC0
 	tailByte  = 0x80
